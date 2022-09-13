eb9f0fdc7fca0f84ea540a8afb41365e04697155
piscesfunny <piscesfunny@gmail.com>
2022-09-13 15:14:36 +0800

    added encoding to write_results_to_csv function

--- utils/helpers.py
+++ utils/helpers.py
@@ -118,12 +118,12 @@ def validate_parameter(argv, parameter_count=1):
         exit(1)
 
 
-def write_results_to_csv(output_f_path, items):
+def write_results_to_csv(output_f_path, items, encoding='utf-8-sig'):
     df = pd.DataFrame(items)
     if os.path.exists(output_f_path):
-        df.to_csv(output_f_path, mode='a', header=False, index=False)
+        df.to_csv(output_f_path, mode='a', header=False, index=False, encoding=encoding)
     else:
-        df.to_csv(output_f_path, index=False)
+        df.to_csv(output_f_path, index=False, encoding=encoding)
 
 
 def read_file(f_path, file_format='csv'):
