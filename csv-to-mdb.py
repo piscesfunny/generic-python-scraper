2a1a69ba3f1c4074810c041bbff1182e4c707e43
piscesfunny <piscesfunny@gmail.com>
2022-09-27 01:53:00 -0700

    added utility module for loading CSV to .mdb

---
+++ csv-to-mdb.py
@@ -0,0 +1,26 @@
+import os
+import pypyodbc
+from utils.config import BASE_DIR, OUTPUT_RESULT_DIR
+
+
+for f in os.listdir(OUTPUT_RESULT_DIR):
+    filename, file_extension = os.path.splitext(f)
+    db_f_path = os.path.join(BASE_DIR, "db", f"{filename}.mdb")
+    if os.path.exists(db_f_path):
+        print(f"Skipped file - {f}")
+        continue
+    pypyodbc.win_create_mdb(db_f_path)
+
+    # DATABASE CONNECTION
+    connection_str = "DRIVER={{Microsoft Access Driver (*.mdb, *.accdb)}};DBQ={};".format(db_f_path)
+    con = pypyodbc.connect(connection_str)
+
+    # RUN QUERY
+    strSQL = f"SELECT * INTO [TableName] FROM [text;HDR=Yes;FMT=Delimited(,);Database={OUTPUT_RESULT_DIR}].{f};"
+    cur = con.cursor()
+    cur.execute(strSQL)
+    con.commit()
+    con.close()
+    print(f"MDB file has been created from {f}")
+
+print("<<<")
