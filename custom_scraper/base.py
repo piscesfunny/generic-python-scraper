b1ecb30c110695cad27cf100fc24b3cc09d7e3b5
pisces <piscesfunny@gmail.com>
2024-01-24 18:41:20 +0800

    feat: add base class for custom scrapers

---
+++ custom_scraper/base.py
@@ -0,0 +1,18 @@
+import os
+
+from utils.config import OUTPUT_LIST_DIR, OUTPUT_RESULT_DIR
+
+
+class CustomBaseSpider(object):
+    def __init__(
+        self,
+        site_name,
+        base_url,
+    ):
+        self.site_name = site_name
+        self.base_url = base_url
+
+        self.item_list_dir = os.path.join(OUTPUT_LIST_DIR, site_name)
+        self.result_dir = os.path.join(OUTPUT_RESULT_DIR, site_name)
+        os.makedirs(self.item_list_dir, exist_ok=True)
+        os.makedirs(self.result_dir, exist_ok=True)
